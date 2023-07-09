9eef3c7522a54cda7c2156319d133c342eb2d231
remimimimimi <remimimimimi@protonmail.com>
2023-07-09 11:08:26 +0300

    Separate to working and not working busybox replacements

--- overlays/busybox.nix
+++ overlays/busybox.nix
@@ -1,9 +1,13 @@
 # Replace common utilites with busybox
 (final: prev: {
-  coreutils = prev.busybox;
-  gnugrep = prev.busybox;
-  findutils = prev.busybox;
-  shadow = prev.busybox;
-  nettools = prev.busybox;
-  util-linux = prev.busybox;
+  # Not working
+  # coreutils = prev.busybox;
+  # gnugrep = prev.busybox;
+  # findutils = prev.busybox;
+  # shadow = prev.busybox // { su = prev.busybox; };
+
+  # Working
+  # nettools = prev.busybox;
+  # util-linux = prev.busybox // { override = {...}: prev.busybox; };
+  # util-linuxMinimal = prev.busybox;
 })
