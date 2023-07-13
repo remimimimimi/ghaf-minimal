e6726985f68d30ed382c20d002793f35d7155458
remimimimimi <remimimimimi@protonmail.com>
2023-07-13 11:30:02 +0300

    Add minimal config

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Ghaf - Documentation and implementation for TII SSRC Secure Technologies Ghaf Framework";
 
   inputs = {
-    nixpkgs.url = "path:./nixpkgs-ghaf";
+    nixpkgs.url = "github:remimimimimi/nixpkgs-ghaf/ghaf-minimal-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
@@ -39,48 +39,96 @@
                 modulesPath,
                 ...
               }: {
+                imports = [
+                  (modulesPath + "/profiles/minimal.nix")
+                ];
+
+                environment.defaultPackages = lib.mkForce [];
+                environment.systemPackages = lib.mkForce [];
+
+                # Keep the only required file in etc to see big dependencies clearly
+                environment.etc = lib.mkForce {
+                  "modprobe.d/nixos.conf".source = ./empty.txt;
+                };
+
+                # nix.enable = false;
+                boot.enableContainers = false;
+                # # FIXME: Save for the bright future
+                boot.initrd.systemd.enable = true;
+                boot.growPartition = lib.mkForce false;
+
                 nixpkgs.overlays = [
-                  (self: super: {
-                    # systemd = super.systemd.override {
-                    #   pname = "systemd-minimal";
-                    #   withAcl = false;
-                    #   withAnalyze = false;
-                    #   withApparmor = false;
-                    #   withAudit = false;
-                    #   # withCompression = false;
-                    #   # withCoredump = false;
-                    #   withCryptsetup = false;
-                    #   withDocumentation = false;
-                    #   withEfi = false;
-                    #   withFido2 = false;
-                    #   withHostnamed = false;
-                    #   withHomed = false;
-                    #   # withHwdb = false;
-                    #   withImportd = false;
-                    #   withLibBPF = false;
-                    #   withLibidn2 = false;
-                    #   withLocaled = false;
-                    #   # withLogind = false;
-                    #   withMachined = false;
-                    #   withNetworkd = false;
-                    #   withNss = false;
-                    #   # withOomd = false;
-                    #   withPCRE2 = false;
-                    #   withPam = false;
-                    #   withPolkit = false;
-                    #   withPortabled = false;
-                    #   withRemote = false;
-                    #   # withResolved = false;
-                    #   withShellCompletions = false;
-                    #   withTimedated = false;
-                    #   # withTimesyncd = false;
-                    #   withTpm2Tss = false;
-                    #   # withUserDb = false;
-                    # };
+                  # (import ./overlays/busybox.nix)
+
+                  (self: super: let
+                    systemd = super.systemd.override {
+                      pname = "systemd-ghaf";
+                      withAcl = false;
+                      withAnalyze = false;
+                      withApparmor = false;
+                      withAudit = false;
+                      withCompression = false;
+                      withCoredump = false;
+                      withCryptsetup = false;
+                      withDocumentation = false;
+                      withEfi = false;
+                      withFido2 = false;
+                      withHostnamed = false;
+                      withHomed = false;
+                      withHwdb = false;
+                      withImportd = false;
+                      withLibBPF = false;
+                      withLibidn2 = false;
+                      withLocaled = false;
+                      withLogind = false;
+                      withMachined = false;
+                      withNetworkd = false;
+                      withNss = false;
+                      withOomd = false;
+                      withPCRE2 = false;
+                      withPam = false;
+                      withPolkit = false;
+                      withPortabled = false;
+                      withRemote = false;
+                      withResolved = false;
+                      withShellCompletions = false;
+                      withTimedated = false;
+                      withTimesyncd = false;
+                      withTpm2Tss = false;
+                      withUserDb = false;
+                    };
+                  in {
+                    # Deduplicate systemd
+                    inherit systemd;
+                    systemdMinimal = systemd;
                   })
                 ];
               })
-              nixos-generators.nixosModules.iso
+
+              nixos-generators.nixosModules.raw-efi
+              ({
+                pkgs,
+                lib,
+                config,
+                modulesPath,
+                ...
+              }: {
+                system.build.raw = lib.mkForce (import "${toString modulesPath}/../lib/make-disk-image.nix" {
+                  inherit lib config pkgs;
+                  partitionTableType = "efi";
+                  diskSize = "auto";
+                  format = "raw";
+                  installBootLoader = false; # TEMPORARY
+                  copyChannel = false;
+                });
+
+                boot.initrd.enable = false;
+
+                # TEMPORARY
+                boot.kernel.enable = false;
+              })
+
+              # nixos-generators.nixosModules.iso
             ];
             inherit system;
           };
@@ -88,9 +136,5 @@
         formatter = pkgs.alejandra;
         packages.default = let cfg = self.nixosConfigurations.${system}.raw.config; in cfg.system.build.${cfg.formatAttr};
       }))
-
-      {
-        lib = import ./lib {inherit self;};
-      }
     ];
 }
--- remount.sh
+++ remount.sh
@@ -13,6 +13,6 @@ OFFSET=$(($START * 512))
 sudo umount $MNT_DIR > /dev/null 2>&1 || true
 sudo mount -o loop,offset=$OFFSET $OUT/nixos.img $MNT_DIR
 
-du -ahd 1 $MNT_DIR/nix/store | sort -rh | perl -e 'my@matches;push(@matches,$&)while(<>=~/^(.+)M/g);print$matches[0]-$matches[1],"M    Nix store size\n";'
+# du -ahd 1 $MNT_DIR/nix/store | sort -rh | perl -e 'my@matches;push(@matches,$&)while(<>=~/^(.+)M/g);print$matches[0]-$matches[1],"M    Nix store size\n";'
 
-du -ahd 1 $MNT_DIR/nix/store | sort -rh | head -n 22 | tail -n 20
+du -ahd 1 $MNT_DIR/nix/store | sort -rh | head -n 21
